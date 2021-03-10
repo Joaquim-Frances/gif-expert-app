f128e3b0b7b442147b7ff3e77dc24c242059fba1
Joaquim-Frances <quimifrances@gmail.com>
2021-03-10 13:02:09 +0100

    Initial Commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const GifExpertApp = () => {
+
+    return (
+        <div>
+            <h2>GiffExpertApp</h2>
+            <hr/>
+        </div>
+    )
+};
+
+export default GifExpertApp;
