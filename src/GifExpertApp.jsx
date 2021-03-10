db822d56805650185b6e3f208232ad715f6ef00d
Quim <quimifrances@gmail.com>
2021-03-10 14:38:31 +0100

    Adding array element through setCategories

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 const GifExpertApp = () => {
 
+    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+
+    const handleAdd = () =>{
+        setCategories([...categories, 'Dr.Slump', 'My Hero Academy']);
+    }
+
     return (
         <div>
-            <h2>GifExpertApp</h2>
+            <h2>Gif Expert App</h2>
             <hr/>
+
+            <button onClick={ handleAdd }>Agregar</button>
+            <input type="text" name="titulo"  id="titulo"/>
+            <br/>
+            <ol>
+                { 
+                    categories.map( category => {
+                        return <li key={category}>{category}</li>
+                    })
+                }
+            </ol>
         </div>
+
     )
 };
 
