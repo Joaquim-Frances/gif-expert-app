093adebc21d8df2d1b20d4ab11eaff3c1ecd8289
Quim <quimifrances@gmail.com>
2021-03-10 22:48:43 +0100

    Custom Hook

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,21 +1,25 @@
-import React, { useState, useEffect, Fragment } from 'react'
-import { getGif } from '../helpers/getGifs';
-import { GifGridItem } from './GifGridItem';
+import React, { Fragment } from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs'
+import { GifGridItem } from './GifGridItem'
+
 
 export const GifGrid = ({category}) => {
     
-    const [images, setImages] = useState([]);
+   
+    const { data, loading } = useFetchGifs( category );
+    
 
-    useEffect ( ()=> {
-        getGif( category ).then( setImages );
-    }, [ category]);
+    // useEffect ( ()=> {
+    //     getGif( category ).then( setImages );
+    // }, [ category]);
     
     return (
         <Fragment>
         <h3> { category } </h3>
+        {loading && <p> Cargando datos...</p>}
         <div className='card-grid'>
                 {
-                    images.map( img =>{
+                    data.map( img =>{
                         return(
                         <GifGridItem 
                         key={ img.id }
--- src/components/GifGridItem.jsx
+++ src/components/GifGridItem.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 
 export const GifGridItem = ( {id, title, url} ) => {
-    
-    
-    console.log(id, title, url);
-
 
     return (
         <div key={id} className='card'>
