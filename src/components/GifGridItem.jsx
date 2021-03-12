a74241a3a585e953251f60326ebe31e4f292035c
Quim <quimifrances@gmail.com>
2021-03-12 10:55:57 +0100

    modified index html

--- src/components/GifGridItem.jsx
+++ src/components/GifGridItem.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const GifGridItem = ( {id, title, url} ) => {
 
     return (
-        <div key={id} className='card animate__animated animate__fadeIn'>
+        <div key={id} className="card animate__animated animate__fadeIn">
             <img src={url} alt={title} />
            
         </div>
