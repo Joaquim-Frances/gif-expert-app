cdfcffaf27d7b090e2f92b43f62aa7e6e3fe9fed
Quim <quimifrances@gmail.com>
2021-03-14 15:52:00 +0100

    Tested GridItem

--- src/components/GifGridItem.jsx
+++ src/components/GifGridItem.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export const GifGridItem = ( {id, title, url} ) => {
 
     return (
         <div key={id} className="card animate__animated animate__fadeIn">
             <img src={url} alt={title} />
-           
+            <p>{title}</p>
         </div>
     )
+}
+
+GifGridItem.propTypes = {
+    id: PropTypes.number,
+    title: PropTypes.string,
+    url: PropTypes.string,
 }
--- src/tests/GifGridItem.test.jsx
+++ src/tests/GifGridItem.test.jsx
@@ -5,18 +5,49 @@ import '@testing-library/jest-dom';
 
 describe('Testing GifItem Component', () => {
 
+    const img = {
+        id: 1,
+        title: 'titulo aleatorio',
+        url: 'https://silencetimer.com'
+    };
+    
+    const wrapper = shallow(<GifGridItem  key={ img.id } { ...img } />);
+
     test('Compononet is rendering properly', () => {
         
-        const img = {
-            id: '1',
-            title: 'titulo aleatorio',
-            url: 'https://silencetimer.com'
-        };
-
-        const wrapper = shallow(<GifGridItem  key={ img.id } { ...img } />);
         expect(wrapper).toMatchSnapshot();
 
     })
+
+    test('should use title prop in the correct place', () => {
+        
+        const p = wrapper.find('p');
+        expect(p.text()).toBe(img.title);
+        
+    })
+
+    test('should have the url image equal to the props url', () => {
+
+        const imagen = wrapper.find('img');
+        expect(imagen.prop("src")).toBe(img.url);
+        expect(imagen.prop("alt")).toBe(img.title);
+        
+    })
+
+    test('should have animate_fadeIn as className', () => {
+        
+        
+        const div = wrapper.find('div');
+        const className = div.prop('className');
+        expect(className.includes('animate__fadeIn')).toBe(true);
+        
+        
+        
+        
+    })
+    
+    
+    
     
     
 })
