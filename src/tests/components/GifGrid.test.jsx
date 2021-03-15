07f108e51b33ab0debef42bdede9396c4f5bbf83
Quim <quimifrances@gmail.com>
2021-03-15 22:36:39 +0100

    Testing GifGrid

--- src/tests/components/GifGrid.test.jsx
+++ src/tests/components/GifGrid.test.jsx
@@ -1,6 +1,8 @@
 import { shallow } from 'enzyme'
 import React from 'react'
 import { GifGrid } from '../../components/GifGrid'
+import { useFetchGifs } from '../../hooks/useFetchGifs';
+jest.mock('../../hooks/useFetchGifs');
 
 
 describe('Testing GifGrid component functions', () => {
@@ -9,17 +11,29 @@ describe('Testing GifGrid component functions', () => {
 
     test('should do a Match with the snapshot', () => {
 
+        useFetchGifs.mockReturnValue({
+            data: [],
+            loading: true,
+        });
+
         const wrapper = shallow( <GifGrid category={ category }/>);
         expect(wrapper).toMatchSnapshot();
         
     })
 
-    test('should show items when images are loaded', () => {
+    test('should show items when images are loaded with our useFetch', () => {
 
-        
-        
+        const gif = [{
+            id: 2,
+            title: 'Shakyamuni',
+            url: 'https://silencetimer.com'
+        }]
+        useFetchGifs.mockReturnValue({
+            data: gif,
+            loading: false,
+        });
+        const wrapper = shallow( <GifGrid category={ category }/>);
+        expect(wrapper).toMatchSnapshot();
     })
-    
-    
     
 })
